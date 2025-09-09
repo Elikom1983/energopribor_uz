d7913f308cefccd8dc47c6ce31735a38d302cfa3
Elikom1983 <65336695+Elikom1983@users.noreply.github.com>
2025-09-09 07:39:50 +0500

    front-page

--- js/main.js
+++ js/main.js
@@ -1,21 +1,49 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
   loop: true,
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 30,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  // 🔹 Avtomatik aylanish
+  autoplay: {
+    delay: 5000, // 3 soniyada almashadi
+    disableOnInteraction: false, // foydalanuvchi bosganda ham davom etadi
   },
 });
+
+
+  var swiper2= new Swiper("#sliderbox", {
+    slidesPerView: 5,  // bir qatorda nechta mahsulot ko‘rinadi
+    spaceBetween: 20,  // mahsulotlar orasidagi masofa
+    loop: true,        // cheksiz aylansin
+     // Navigation arrows
+  navigation: {
+    nextEl: '#swiper-button-next',
+    prevEl: '#swiper-button-prev',
+  },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+
+  var swiper3 = new Swiper("#sliderbox2", {
+    slidesPerView: 5,  // bir qatorda nechta mahsulot ko‘rinadi
+    spaceBetween: 20,  // mahsulotlar orasidagi masofa
+    loop: true,        // cheksiz aylansin
+     // Navigation arrows
+  navigation: {
+    nextEl: '#swiper-button-next2',
+    prevEl: '#swiper-button-prev2',
+  },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false, 
+    },
+  });
+   
